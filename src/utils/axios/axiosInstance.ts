7635deb96b12aba69286c68f153ba5c0619f0a74
vorpal13 <aydarbaev.bekpolat@gmail.com>
2024-11-24 22:02:37 +0500

    changed base url for api

--- src/utils/axios/axiosInstance.ts
+++ src/utils/axios/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: import.meta.env.VITE_BASE_URL,
 })
 
 axiosInstance.interceptors.request.use((config) => {
